b862d33bc49e29b8419e5942307013a7ee27d617
mohamed <mohamedabdelhakem476@gmail.com>
2020-12-15 21:22:46 +0200

    pacifistAgent

---
+++ src/agents/PacificAgent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class pacifistAgent {
+    constructor(props){
+
+    }
+    /**
+     * function AssignArmy()
+     * 
+     * 
+     * 
+     * 
+     * 
+     *  
+     * */     
+    AssignArmy = (player, map)=>{
+       let ganed = player.getCurrentTerritories()/;
+
+    }           
+}
